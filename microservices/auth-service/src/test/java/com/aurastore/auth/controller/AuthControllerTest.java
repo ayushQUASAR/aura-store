2818ce39b6ca70318c8d0223b06c2d950e7224fa
AuraStore <aurastore@dev.local>
2026-07-30 04:36:24 +0000

    fix: use @MockBean instead of @MockitoBean for Spring Boot 3.2 compatibility

--- microservices/auth-service/src/test/java/com/aurastore/auth/controller/AuthControllerTest.java
+++ microservices/auth-service/src/test/java/com/aurastore/auth/controller/AuthControllerTest.java
@@ -6,7 +6,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.http.MediaType;
-import org.springframework.test.context.bean.override.mockito.MockitoBean;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.web.servlet.MockMvc;
 
 import java.util.Map;
@@ -26,7 +26,7 @@ class AuthControllerTest {
     @Autowired
     private ObjectMapper objectMapper;
 
-    @MockitoBean
+    @MockBean
     private UserRepository userRepository;
 
     @Test
